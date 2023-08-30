9d183fe35bb62a3d0d1c10a15e5e860d5ea6b89a
aristo-13 <oseirichmond310@gmail.com>
2023-08-30 00:53:09 +0000

    new

---
+++ script.js
@@ -0,0 +1,136 @@
+const words = [
+    "APPLE",
+    "TABLE",
+    "BREAD",
+    "SWORD",
+    "FRUIT",
+    "WATER",
+    "HAPPY",
+    "PLANT",
+    "STARS",
+    "SMILE",
+    "MOUSE",
+    "FLOOR",
+    "CHAIR",
+    "LIGHT",
+    "MUSIC",
+    "GRASS",
+    "HEART",
+    "ROCKS",
+    "SKYPE",
+    "FIRED"
+];
+
+const refeshbtn = document.querySelector('.refresh')
+const checkbtn = document.querySelector('.check')
+const UserInput = document.querySelector('.input-box')
+const shuffle = document.querySelector('.shuffled-word')
+const feedback = document.querySelector('.feedback')
+let shuffledWord 
+let word
+let a
+let b
+let timeIN
+let is_done = false
+
+refeshbtn.addEventListener('click', (e) =>{
+    e.target.textContent = 'Refresh'
+    timing()
+    refeshWord()
+})
+
+const timer = document.querySelector('.timer')
+timeValue = 30
+
+function timing(){
+      clearInterval(timeIN) 
+     timeIN = setInterval(() =>{
+         timeValue--
+         if(timeValue < 0){
+            refeshWord()
+         }
+         timer.textContent = `Time left: ${timeValue}s`
+    },1000)
+}
+
+
+function refeshWord(){
+    timeValue = 30
+    timing()
+
+    shuffle.textContent = ''
+    UserInput.value = ''
+    a = []
+    b = []
+
+    let ran = Math.floor(Math.random() * words.length)
+    word = words[ran]
+    for(let i = 0; i < word.length; i++){
+        a.push(word[i])
+    }
+    for(let i = 0; i < a.length; i++){
+        b.splice(Math.floor(Math.random() * words.length),0,a[i])
+    }
+    shuffledWord = b.join('')
+
+   if(shuffledWord == word){
+    refeshWord()
+   }
+   shuffle.textContent = shuffledWord
+}
+
+
+checkbtn.addEventListener('click', () =>{
+    if(UserInput.value !== '')
+        checkRes()
+    else enterWord()
+})
+function checkRes(){
+    clearInterval(timeIN)
+    let userIN = (UserInput.value).toUpperCase()
+    
+    document.querySelector(".result").style.display  = 'block'
+    if(userIN == word){
+        document.querySelector(".material-symbols-outlined").textContent = 'check'
+        feedback.textContent = 'YOU WIN'
+    }else{
+        document.querySelector(".material-symbols-outlined").textContent = 'close'
+        feedback.textContent = 'YOU LOST'
+    }
+}
+
+
+UserInput.addEventListener('keydown', (event) =>{
+    if(event.key == 'Enter'){
+        if(UserInput.value !== '')
+           checkRes()
+    else enterWord()
+    }
+})
+
+
+document.addEventListener('click', (e) =>{
+    if(e.target === document.querySelector('body') || e.target === document.querySelector('.container')){
+        document.querySelector(".result").style.display  = 'none'
+        refeshWord()
+    }
+})
+
+
+let invalid = document.querySelector('.invalid')
+function enterWord(){
+    let line = document.querySelector('.line')
+    invalid.classList.add('active')
+    line.classList.add('active')
+    
+
+    if(line.style.width == '0'){
+      invalid.style.display = 'none'
+    }
+}
+
+let closer = document.querySelector('.close')
+
+closer.addEventListener('click', () =>{
+  invalid.style.right = '-100%'
+})
